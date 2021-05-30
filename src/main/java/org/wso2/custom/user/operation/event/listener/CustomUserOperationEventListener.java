624d6f4b2ccc810bde4ae168ce32a851d7b60517
DimuthuKasunWP <dimuthuk@wso2.com>
2021-05-30 12:54:02 +0530

    override getexecutionorderid method

--- src/main/java/org/wso2/custom/user/operation/event/listener/CustomUserOperationEventListener.java
+++ src/main/java/org/wso2/custom/user/operation/event/listener/CustomUserOperationEventListener.java
@@ -1,9 +1,16 @@
 package org.wso2.custom.user.operation.event.listener;
 
 
+import org.wso2.carbon.user.api.Permission;
 import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.common.*;
+import org.wso2.carbon.user.core.model.Condition;
+import org.wso2.carbon.user.core.model.UniqueIDUserClaimSearchEntry;
+import org.wso2.carbon.user.core.model.UserClaimSearchEntry;
+
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -12,6 +19,11 @@ import org.wso2.carbon.user.core.common.*;
 public class CustomUserOperationEventListener extends AbstractUserOperationEventListener {
 
     private String systemUserPrefix = "system_";
+
+
+    public CustomUserOperationEventListener() {
+        super();
+    }
 
     @Override
     public boolean doPreDeleteUserWithID
@@ -25,6 +37,10 @@ public class CustomUserOperationEventListener extends AbstractUserOperationEvent
     }
 
 
+    @Override
+    public int getExecutionOrderId() {
+        return 9000;
+    }
 
     @Override
     public boolean doPreDeleteUser
@@ -36,6 +52,5 @@ public class CustomUserOperationEventListener extends AbstractUserOperationEvent
             return true;
         }
     }
-
 
 }
